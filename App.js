8d84ab5f13a8c70778c00a577970be9b242a5611
Brent Vatne <brentvatne@gmail.com>
2023-12-05 18:19:14 -0800

    repro

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
